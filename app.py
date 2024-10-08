0ca2e61ddad70600ef913a6c617255e12a61bea6
Eddi García <eddi.garciab@gmail.com>
2024-10-08 12:45:17 -0500

    Initial commit

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import json
 import qrcode
 from io import BytesIO
 from email.mime.image import MIMEImage
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -109,4 +110,5 @@ def send_thank_you_email():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
